fdaf0ac34282164144b72b79ffe9227a17713b2e
Byron Veale <veale@princeton.edu>
2019-06-15 08:33:34 -0400

    Clean-sheet approach to single menu design.

---
+++ assets/simple_single_menu.js
@@ -0,0 +1,156 @@
+$(document).ready(function() {
+
+    var menuNav = $('nav.menu');
+
+    var menuButton = menuNav.children('button#menu_button');
+
+    var menuBar = menuButton.next('ul');
+
+    var menuListItems = menuBar.children('li');
+
+    var menuLinks = menuListItems.children('a');
+
+    /**
+     * Initialize menu items.
+     */
+    menuButton
+        .addClass('expandable')
+        .attr('aria-expanded', 'false')
+    ;
+
+    menuBar
+        .addClass('menu-bar expandable')
+        .attr('aria-expanded', 'false')
+        .attr('aria-haspopup', 'true')
+        .attr('aria-hidden', 'true')
+        .attr('aria-labeledby', 'menu_button')
+        .attr('role', 'menubar')
+    ;
+
+    menuListItems
+        .addClass('menu-list-item')
+        .first()
+        .addClass('first')
+    ;
+    menuListItems
+        .last()
+        .addClass('last')
+    ;
+
+    menuLinks
+        .addClass('menu-item')
+        .attr('role', 'menuitem')
+        .attr('tabindex', '-1')
+    ;
+
+    menuButton.on('focus mouseenter', function(event) {
+        showMenu(menuButton, menuBar, menuListItems, menuLinks);
+    });
+
+    function showMenu(button, bar, items, links){
+        button
+            .addClass('expanded')
+            .attr('aria-expanded', 'true')
+        ;
+
+        bar
+            .addClass('expanded')
+            .attr('aria-expanded', 'true')
+            .attr('aria-hidden', 'false')
+        ;
+    }
+
+    menuNav.on('mouseleave', function(event) {
+        hideMenu(menuButton, menuBar, menuListItems, menuLinks);
+    });
+
+    function hideMenu(button, bar, items, links){
+        button
+            .removeClass('expanded')
+            .attr('aria-expanded', 'false')
+        ;
+
+        bar
+            .removeClass('expanded')
+            .attr('aria-expanded', 'false')
+            .attr('aria-hidden', 'true')
+        ;
+    }
+
+    menuNav.keydown(function(event){
+        switch (event.which) {
+            case 9: // Tab key
+                hideMenu(menuButton, menuBar, menuListItems, menuLinks);
+                $(this).blur();
+                break;
+
+            case 27: // Escape key
+                hideMenu(menuButton, menuBar, menuListItems, menuLinks);
+                break;
+        }
+    });
+
+    menuButton.keydown(function(event){
+        switch (event.which) {
+            case 13: // Return key
+                if ($(this).hasClass('expanded')) {
+                    hideMenu(menuButton, menuBar, menuListItems, menuLinks);
+                }
+                else {
+                    showMenu(menuButton, menuBar, menuListItems, menuLinks);
+                }
+                break;
+
+            case 40: // Down arrow
+                event.preventDefault();
+                event.stopPropagation();
+                $(this)
+                    .next('ul.looking-for')
+                    .children('li')
+                    .eq(0)
+                    .children('a.menu-item')
+                    .focus()
+                ;
+                break;
+        }
+    });
+
+    menuLinks.keydown(function(event){
+        switch (event.which) {
+            case 40: // Down arrow
+                event.preventDefault();
+                event.stopPropagation();
+                // If last list item, set focus on first item.
+                if ($(this).parent('li').hasClass('last')) {
+                    $(this).parents('ul.menu-bar').children('li.first').children('a').focus();
+                } else {
+                    $(this)
+                        .parent('li.menu-list-item')
+                        .next('li.menu-list-item')
+                        .children('a.menu-item')
+                        .focus()
+                    ;
+                }
+                break;
+
+            case 38: // Up arrow
+                event.preventDefault();
+                event.stopPropagation();
+                // If first list item, set focus on last item.
+                if ($(this).parent('li').hasClass('first')) {
+                    $(this).parents('ul.menu-bar').children('li.last').children('a').focus();
+                } else {
+                    $(this)
+                        .parent('li')
+                        .prev('li')
+                        .children('a')
+                        .focus()
+                    ;
+                }
+                break;
+
+        }
+    });
+
+
+});
